8603fd903e05fe5e4a9ca84cfe833fb715e2ea61
Abhishek Modi <abhishek.modi138@gmail.com>
2019-09-12 09:37:38 +0530

    Updating

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -18,7 +18,7 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
-        out.write("hello world This is demo Of Java Tomcat".getBytes());
+        out.write("hello world This is  demo Of Java Tomcat".getBytes());
         out.flush();
         out.close();
     }
